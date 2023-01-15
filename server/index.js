4294e0ac6b56a9d54551468efffe4022811546a0
Maciej <maciekwoj4@gmail.com>
2023-01-16 00:16:06 +0100

    graphql config

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.use("/graphql", graphqlHTTP({schema, graphiql: process.env.NODE_ENV === "development"}))
+app.use(
+  "/graphql",
+  graphqlHTTP({schema, graphiql: process.env.NODE_ENV === "development"})
+)
 
 app.listen(port, console.log(`running on port ${port}`))
