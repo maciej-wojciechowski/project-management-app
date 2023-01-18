d1f173702dc1296fbb20fa38124ed14c7ca8bc09
Maciej <maciekwoj4@gmail.com>
2023-01-18 23:20:07 +0100

    server cont

---
+++ server/models/Client.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const ClientSchema = new mongoose.Schema({
+  name: {
+    String,
+  },
+  email: {
+    String,
+  },
+  phone: {
+    String,
+  },
+})
+
+module.exports = mongoose.model("Client", ClientSchema)
